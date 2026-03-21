fe119df9e50b8c3af06f9623b440385b5f6116df
highlrang <59989932+highlrang@users.noreply.github.com>
2026-03-21 14:44:43 +0900

    fix. swagger

--- src/main/kotlin/com/hwcompany/fortune_index/common/OpenApiConfig.kt
+++ src/main/kotlin/com/hwcompany/fortune_index/common/OpenApiConfig.kt
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
 import io.swagger.v3.oas.models.security.SecurityRequirement
 import io.swagger.v3.oas.models.security.SecurityScheme
+import io.swagger.v3.oas.models.servers.Server
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -19,6 +20,7 @@ class OpenApiConfig {
                     .description("Fortune Index API 문서")
                     .version("v1")
             )
+            .servers(listOf(Server().url("/")))
             .components(
                 Components()
                     .addSecuritySchemes(
