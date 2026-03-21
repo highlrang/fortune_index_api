b333cc156266c1bf667eea586ae7e9e6317c3fc9
highlrang <59989932+highlrang@users.noreply.github.com>
2026-03-21 12:59:24 +0900

    fix. yml

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
-    kotlin("jvm") version "2.2.21"
-    kotlin("plugin.spring") version "2.2.21"
+    kotlin("jvm") version "1.9.24"
+    kotlin("plugin.spring") version "1.9.24"
     id("org.springframework.boot") version "3.3.5"
     id("io.spring.dependency-management") version "1.1.7"
-    kotlin("plugin.jpa") version "2.2.21"
+    kotlin("plugin.jpa") version "1.9.24"
 }
 
 group = "com.hwcompany"
