1ca010b9e34235aa0f15b70df61a64979bd54c93
highlrang <59989932+highlrang@users.noreply.github.com>
2026-03-25 21:38:42 +0900

    fix. tarot enum

--- src/main/kotlin/com/hwcompany/fortune_index/tarot/TarotCatalogEntities.kt
+++ src/main/kotlin/com/hwcompany/fortune_index/tarot/TarotCatalogEntities.kt
@@ -1,18 +1,6 @@
 package com.hwcompany.fortune_index.tarot
 
-import jakarta.persistence.AttributeConverter
-import jakarta.persistence.Column
-import jakarta.persistence.Convert
-import jakarta.persistence.Entity
-import jakarta.persistence.FetchType
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
-import jakarta.persistence.Index
-import jakarta.persistence.JoinColumn
-import jakarta.persistence.ManyToOne
-import jakarta.persistence.Table
-import jakarta.persistence.Converter
+import jakarta.persistence.*
 import java.time.LocalDateTime
 
 @Entity
